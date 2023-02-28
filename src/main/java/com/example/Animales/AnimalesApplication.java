d3bb0f0d61c863db6982092187af2cb73a187b62
AnaMCorredor <sunnyanma@gmail.com>
2023-02-28 15:08:27 -0500

    versionAna

--- src/main/java/com/example/Animales/AnimalesApplication.java
+++ src/main/java/com/example/Animales/AnimalesApplication.java
@@ -1,13 +1,48 @@
 package com.example.Animales;
 
+import Controlador.ControladorAna;
+import Controlador.ControladorMichael;
+import Controlador.ControladorOmar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.util.Scanner;
 @SpringBootApplication
 public class AnimalesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimalesApplication.class, args);
-	}
+		System.out.println("\n --- BIENVENIDO A LA REGISTRADURIA PARA MASCOTAS ---");
+		Scanner op = new Scanner(System.in);
 
-}
+		ControladorAna conAna = new ControladorAna();
+		ControladorMichael conMi = new ControladorMichael();
+		ControladorOmar conOm = new ControladorOmar();
+
+		int opcion = 0;
+
+
+
+		do{
+			System.out.println("\n MENU DE OPCIONES");
+			System.out.println("\n[1] REGISTRO MASCOTAS");
+			System.out.println("\n[2] CITAS MASCOTAS");
+			System.out.println("\n[3] ENFERMEDADES MASCOTAS");
+			System.out.println("\n[4] SALIR");
+			System.out.println("\nIngresar opcion: ");
+			opcion = op.nextInt();
+
+			switch(opcion){
+				case 1:
+					conAna.getOpcion();
+					break;
+
+				case 2:
+					break;
+
+				case 3:
+					break;
+
+			}
+
+		}while(opcion !=4);}
+	}
