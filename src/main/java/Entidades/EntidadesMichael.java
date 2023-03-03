f4e78e6c6aaf60b714bb5c3207944bb9c99c7f7b
Michael-Lozano <maicolozano8@gmail.com>
2023-03-03 10:17:02 -0500

    Corrigiendo errores

--- src/main/java/Entidades/EntidadesMichael.java
+++ src/main/java/Entidades/EntidadesMichael.java
@@ -1,4 +1,41 @@
 package Entidades;
 
 public class EntidadesMichael {
+
+    private String NombreMas;
+    private int DocumentoMas;
+    private String Fecha;
+
+    public EntidadesMichael(String nombreMas, int documentoMas, String fecha) {
+        NombreMas = nombreMas;
+        DocumentoMas = documentoMas;
+        Fecha = fecha;
+    }
+
+    public EntidadesMichael() {
+    }
+
+    public String getNombreMas() {
+        return NombreMas;
+    }
+
+    public void setNombreMas(String nombreMas) {
+        NombreMas = nombreMas;
+    }
+
+    public int getDocumentoMas() {
+        return DocumentoMas;
+    }
+
+    public void setDocumentoMas(int documentoMas) {
+        DocumentoMas = documentoMas;
+    }
+
+    public String getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(String fecha) {
+        Fecha = fecha;
+    }
 }
