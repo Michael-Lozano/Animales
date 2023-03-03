127a448d5abe015431a450526e0dfe588d4836fd
Michael-Lozano <maicolozano8@gmail.com>
2023-03-03 07:16:33 -0500

    Subiendo mi parte al master Michael

--- src/main/java/Servicios/ServiciosMichael.java
+++ src/main/java/Servicios/ServiciosMichael.java
@@ -1,4 +1,53 @@
 package Servicios;
 
+import Entidades.EntidadesMichael;
+
+import java.util.ArrayList;
+
 public class ServiciosMichael {
+
+    EntidadesMichael EntMichael = new EntidadesMichael();
+
+    private ArrayList<EntidadesMichael> Entidades;
+
+    public ServiciosMichael() {
+
+        Entidades = new ArrayList<>();
+
+    }
+
+    public void AgregarMichael(String NombreMas, int DocumentoMas, String Fecha) {
+
+        EntidadesMichael nuevo = new EntidadesMichael(NombreMas, DocumentoMas, Fecha);
+
+        Entidades.add(nuevo);
+
+        System.out.println(" DATOS GUARDADOS ");
+
+    }
+
+    public void EliminarMichael(int Documentos) {
+
+        for (int i = 0; i < Entidades.size(); i++) {
+
+            Entidades.remove(i);
+
+            System.out.println(" ELIMINO ");
+
+        }
+
+    }
+
+    public void Buscar() {
+
+        for (int i = 0; i < Entidades.size(); i++) {
+
+            System.out.println("--------------------------");
+            System.out.println("Nombre: " + Entidades.get(i).getNombreMas()+  "\n");
+            System.out.println("Documento: " + Entidades.get(i).getDocumentoMas()+  "\n");
+            System.out.println("Fecha: " + Entidades.get(i).getFecha()+  "\n");
+
+        }
+    }
+
 }
--- src/main/java/com/example/Animales/AnimalesApplication.java
+++ src/main/java/com/example/Animales/AnimalesApplication.java
@@ -1,13 +1,23 @@
 package com.example.Animales;
 
+import Controlador.ControladorMichael;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class AnimalesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AnimalesApplication.class, args);
+
+
+
+
+
+
 	}
 
 }
